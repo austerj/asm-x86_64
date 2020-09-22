9513bbd8b35c146eca21643d68e8f29988fdcf21
austerj <johanauster@gmail.com>
2020-09-22 22:37:39 +0200

    math operations and stack

---
+++ 04_stack/math.asm
@@ -0,0 +1,26 @@
+section .data
+        digit db 0,10
+
+section .text
+        global _start
+
+_start:
+        mov rax, 6
+        add rax, 2
+        mov rbx, 2
+        div rbx
+        call _printRAXDigit        
+
+        mov rax, 60
+        mov rdi, 0
+        syscall
+
+_printRAXDigit:
+        add rax, 48
+        mov [digit], al
+        mov rax, 1
+        mov rdi, 1
+        mov rsi, digit
+        mov rdx, 2
+        syscall
+        ret
